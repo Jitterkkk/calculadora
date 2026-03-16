7d0eea4137f86ad323c8f548df89b0cd3093ca8c
Jitterkkk <jitterzibetti@gmail.com>
2026-03-15 22:48:24 -0300

    Identação no JavaScript

--- static/script.js
+++ static/script.js
@@ -1,19 +1,34 @@
-const display = document.getElementById("display");
-const buttons = document.querySelectorAll(".btn");
+const display = document.getElementById("display")
 
-buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const value = btn.innerText;
-    if (value === "C") {
-      display.value = "";
-    } else if (value === "=") {
-      try {
-        display.value = eval(display.value);
-      } catch {
-        display.value = "Erro";
-      }
-    } else {
-      display.value += value;
-    }
-  });
-});
+const buttons = document.querySelectorAll("button")
+
+buttons.forEach(button => {
+
+button.addEventListener("click", () => {
+
+const value = button.innerText
+
+if(value === "C"){
+display.value = ""
+}
+
+else if(value === "="){
+
+try{
+display.value = eval(display.value)
+}
+catch{
+display.value = "Erro"
+}
+
+}
+
+else{
+
+display.value += value
+
+}
+
+})
+
+})
